a36da9dd43c841f9c0a3955e54a7e04e9ed9645f
matthew lee <matthewlee07@gmail.com>
2018-05-01 10:48:15 -0400

    added host to db

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 exports.DATABASE_URL =
     process.env.DATABASE_URL ||
-    global.DATABASE_URL ||
-    'mongod://localhost/boardgameDB';
+    global.DATABASE_URL
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'password';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
