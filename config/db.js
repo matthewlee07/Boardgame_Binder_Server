a4b3025936b2126283ac5a85adfecf9b8bc06bcb
matthew lee <matthewlee07@gmail.com>
2018-05-01 11:34:32 -0400

    meh

--- config/db.js
+++ config/db.js
@@ -1,10 +1,6 @@
 let Sequelize = require('sequelize');
 console.log(process.env.DATABASE_URL);
 let sequelize = new Sequelize(process.env.DATABASE_URL, {
-    // dialect: 'postgres',
-    // protocol: 'postgres',
-    // port: match[4],
-    // host: match[3],
     logging: true, //false
     quoteIdentifiers: true,
 
