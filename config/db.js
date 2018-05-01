28ef4d020bf805a26e08e7464e0aea15bf0b7708
matthew lee <matthewlee07@gmail.com>
2018-05-01 10:55:49 -0400

    removed host

--- config/db.js
+++ config/db.js
@@ -1,8 +1,7 @@
 let Sequelize = require('sequelize');
 
-let sequelize = new Sequelize('mainDB', null, null, {
+let sequelize = new Sequelize(process.env.DATABASE_URL, null, null, {
     dialect: "postgres",
-    host:'lksgvhqwhstssm:c3c9f162b9f1be29b20faf419f06abbe722cc379b21f8781d01c0be58014648b@ec2-54-204-46-236.compute-1.amazonaws.com'
 });
 
 sequelize.sync();
