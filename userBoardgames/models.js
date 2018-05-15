be76992fb80fe7d5e268e64bdb2215ba5888d28c
matthew lee <matthewlee07@gmail.com>
2018-05-15 10:40:13 -0400

    details changed dot to _

--- userBoardgames/models.js
+++ userBoardgames/models.js
@@ -5,14 +5,14 @@ let UserBoardGame = sequelize.define("UserBoardGame", {
     "id": { type: Sequelize.INTEGER, primaryKey: true, field: 'UserBoardGame.id' },
     "userID": { type: Sequelize.INTEGER },
     "boardGameID": { type: Sequelize.INTEGER },
-    "description": { type: Sequelize.STRING, field: 'details.description' },
-    "image": { type: Sequelize.STRING, field: 'details.image' },
-    "numplayers": { type: Sequelize.INTEGER, field: 'details.minplayers' },
-    "minplayers": { type: Sequelize.INTEGER, field: 'details.minplayers' },
-    "maxplayers": { type: Sequelize.INTEGER, field: 'details.maxplayers' },
-    "playingtime": { type: Sequelize.INTEGER, field: 'details.playingtime' },
-    "name": { type: Sequelize.STRING, field: 'details.name' },
-    "rating": { type: Sequelize.INTEGER, field: 'stats.average' },
+    "description": { type: Sequelize.STRING, field: 'details_description' },
+    "image": { type: Sequelize.STRING, field: 'details_image' },
+    "numplayers": { type: Sequelize.INTEGER, field: 'details_minplayers' },
+    "minplayers": { type: Sequelize.INTEGER, field: 'details_minplayers' },
+    "maxplayers": { type: Sequelize.INTEGER, field: 'details_maxplayers' },
+    "playingtime": { type: Sequelize.INTEGER, field: 'details_playingtime' },
+    "name": { type: Sequelize.STRING, field: 'details_name' },
+    "rating": { type: Sequelize.INTEGER, field: 'stats_average' },
 });
 
 module.exports = UserBoardGame
