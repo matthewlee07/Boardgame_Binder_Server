69875d87bf960ac349bbce266cb8ec9086dea415
matthew lee <matthewlee07@gmail.com>
2018-05-17 13:53:33 -0400

    orderb by ASC

--- boardgames/router.js
+++ boardgames/router.js
@@ -8,23 +8,6 @@ const Op = Sequelize.Op;
 router.get('/', (req, res) => {
     let page = req.query.page || 0;
     let pageSize = req.query.pageSize || 10;
-    // Sequelize.query[`select game_id as id, details_description as description, details_image as image,
-    //     details_minplayers as minplayers, details_maxplayers as maxplayers, details_playingtime as playingtime,
-    //     details_name as name, stats_average as rating from "BoardGames" where details_name like :name and
-    //     details_minplayers > :minplayers and details_maxplayers < :maxplayers and details_playingtime between :minplayingtime
-    //     and :maxplayingtime and stats_average between :minrating and :maxrating limit :limit`, {
-    //         replacements: {
-    //             name: "%" + (req.query.name || "game") + "%",
-    //             minplayers: req.query.minplayers || 1,
-    //             maxplayers: req.query.maxplayers || 20,
-    //             minplayingtime: req.query.minplayingtime || 0,
-    //             maxplayingtime: req.query.maxplayingtime || 1000,
-    //             minrating: req.query.minrating || 0,
-    //             maxrating: req.query.maxrating || 10,
-    //             limit: pageSize
-    //         },
-    //         model: BoardGame
-    //     }]
     BoardGame
         .findAll({
             limit: pageSize,
@@ -47,7 +30,7 @@ router.get('/', (req, res) => {
                 }
             },
             order: [
-                ['details_name', 'DESC']
+                ['details_name', 'ASC']
             ]
         })
         .then(games => {
