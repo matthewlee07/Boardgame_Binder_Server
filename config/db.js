71a76f48f3b3086e325ac6ecf2180a6c62a12f05
matthew lee <matthewlee07@gmail.com>
2018-07-05 16:39:21 -0400

    .

--- config/db.js
+++ config/db.js
@@ -2,11 +2,12 @@ let Sequelize = require('sequelize');
 let sequelize = new Sequelize({
     username: 'matthewlee',
     password: 'Freecoding18',
+    database: 'boardgames',
     dialect: "postgres",
     storage: './db/BoardGames.sql',
     // operatorsAliases: false
 });
 
 sequelize.sync();
-
+// sequelize.sync({force: true})
 module.exports = sequelize
