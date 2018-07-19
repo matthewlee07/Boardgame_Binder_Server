ef218b0c33e91004a9c91036a8e99daf242e6e85
matthew lee <matthewlee07@gmail.com>
2018-07-19 04:48:59 -0400

    .

--- test/test-boardgames.js
+++ test/test-boardgames.js
@@ -1,10 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
-const should = chai.should();
 const chaiHttp = require('chai-http');
 const { app, runServer, closeServer } = require('../server');
-
-
 chai.use(chaiHttp);
 
 describe('/boardgames', () => {
@@ -16,7 +13,7 @@ describe('/boardgames', () => {
         return closeServer();
     })
 
-    describe('GET', function () {
+    describe('GET', () => {
         const sample_boardgame = {
             id: 1,
             description: "Die Macher is a game about seven sequential political races in different regions of Germany. Players are in charge of national political parties, and must manage limited resources to help their party to victory. The winning party will have the most victory points after all the regional elections. There are four different ways of scoring victory points. First, each regional election can supply one to eighty victory points, depending on the size of the region and how well your party does in it. Second, if a party wins a regional election and has some media influence in the region, then the party will receive some media-control victory points. Third, each party has a national party membership which will grow as the game progresses and this will supply a fair number of victory points. Lastly, parties score some victory points if their party platform matches the national opinions at the end of the game.&#10;&#10;The 1986 edition featured 4 parties from the old West Germany and supported 3-4 players. The 1997 edition supports up to 5 players in the re-united Germany and updated several features of the rules as well. The 2006 edition also supports up to 5 players and adds a shorter 5 round variant and additional rules updates by the original designer.&#10;&#10;Die Macher is #1 in the Valley Games Classic Line&#10;&#10;",
@@ -29,16 +26,15 @@ describe('/boardgames', () => {
         }
 
         describe('GET', function () {
-
-            it('Should return all existing boardgames', function () {
+            it('Should return all existing boardgames', () => {
                 return chai
                     .request(app)
                     .get('/boardgames')
-                    .then(function (res) {
+                    .then(res => {
                         expect(res).to.have.status(200);
                         expect(res.body.length).to.be.above(0);
                         expect(res.body).to.be.an('array');
-                        res.body.forEach(function (boardgame) {
+                        res.body.forEach(boardgame => {
                             expect(boardgame).to.be.an('object');
                             expect(boardgame).to.include.keys(
                                 'id', 'description', 'image', 'maxplayers', 'minplayers', 'name', 'playingtime', 'rating');
@@ -52,11 +48,11 @@ describe('/boardgames', () => {
                     });
             });
 
-            it('Should return boardgame ID = 1', function () {
+            it('Should return boardgame ID = 1', () => {
                 return chai
                     .request(app)
                     .get('/boardgames/1')
-                    .then(function (res) {
+                    .then(res => {
                         expect(res).to.have.status(200);
                         expect(res).to.be.an('object');
                         expect(res.body).to.include.keys(
@@ -68,7 +64,12 @@ describe('/boardgames', () => {
                         expect(res.body.avgTime).to.deep.equal(sample_boardgame.avgTime);
                         expect(res.body.rating).to.deep.equal(sample_boardgame.rating);
                         expect(res.body.imgUrl).to.deep.equal(sample_boardgame.imgUrl);
-                    });
+                    })
+                    .catch(err => {
+                        if (err instanceof chai.AssertionError) {
+                            throw err;
+                        }
+                    });;
             });
         });
     })
--- test/test-users.js
+++ test/test-users.js
@@ -1,19 +1,112 @@
 const chai = require('chai');
 const expect = chai.expect;
-const should = chai.should();
 const chaiHttp = require('chai-http');
 const { app, runServer, closeServer } = require('../server');
-let Sequelize = require('sequelize');
 chai.use(chaiHttp);
 
 describe('/users', () => {
     before(() => {
-        Users.sync({ force: true })
         return runServer();
     });
-    console.log('testing users')
+
     after(() => {
         return closeServer();
         console.log('closing server')
+    })
+
+    const sample_user = {
+        userName: "userName",
+        firstName: "firstName",
+        lastName: "lastName",
+        email: "email@email.com",
+        password: "password123"
+    }
+
+    describe('GET', () => {
+        it('Should return all existing users', () => {
+            return chai
+                .request(app)
+                .get('/users')
+                .then(res => {
+                    expect(res).to.have.status(200);
+                    expect(res.body.length).to.be.above(0);
+                    expect(res.body).to.be.an('array');
+                    res.body.forEach(user => {
+                        expect(user).to.be.an('object');
+                        expect(user).to.include.keys(
+                            'userName', 'firstName', 'lastName', 'email');
+                    });
+                    expect(res).to.be.json;
+                })
+                .catch(err => {
+                    if (err instanceof chai.AssertionError) {
+                        throw err;
+                    }
+                });
+        })
+        it('Should return user ID = 1', () => {
+            return chai
+                .request(app)
+                .get('/users/1')
+                .then(res => {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.an('object');
+                    expect(res.body).to.include.keys(
+                        'userName', 'firstName', 'lastName', 'email');
+                    expect(res.body.userName).to.deep.equal(sample_user.userName);
+                    expect(res.body.firstName).to.deep.equal(sample_user.firstName);
+                    expect(res.body.lastName).to.deep.equal(sample_user.lastName);
+                    expect(res.body.email).to.deep.equal(sample_user.email);
+                })
+                .catch(err => {
+                    if (err instanceof chai.AssertionError) {
+                        throw err;
+                    }
+                });;
+        });
+    })
+
+    describe('GET', () => {
+        it('Should return all existing users', () => {
+            return chai
+                .request(app)
+                .get('/users')
+                .then(res => {
+                    expect(res).to.have.status(200);
+                    expect(res.body.length).to.be.above(0);
+                    expect(res.body).to.be.an('array');
+                    res.body.forEach(user => {
+                        expect(user).to.be.an('object');
+                        expect(user).to.include.keys(
+                            'userName', 'firstName', 'lastName', 'email');
+                    });
+                    expect(res).to.be.json;
+                })
+                .catch(err => {
+                    if (err instanceof chai.AssertionError) {
+                        throw err;
+                    }
+                });
+        })
+        it('Should return user ID = 1', () => {
+            return chai
+                .request(app)
+                .get('/users/1')
+                .then(res => {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.an('object');
+                    expect(res.body).to.include.keys(
+                        'userName', 'firstName', 'lastName', 'email');
+                    expect(res.body.userName).to.deep.equal(sample_user.userName);
+                    expect(res.body.firstName).to.deep.equal(sample_user.firstName);
+                    expect(res.body.lastName).to.deep.equal(sample_user.lastName);
+                    expect(res.body.email).to.deep.equal(sample_user.email);
+                })
+                .catch(err => {
+                    if (err instanceof chai.AssertionError) {
+                        throw err;
+                    }
+                });;
+        });
     })
 });
